08bada4bf87379254a8681f59e372e916794f0cd
priya <priya.choudhary@tech9.com>
2022-11-30 13:09:09 +0530

    Adding and removing login cookie explicitly

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useCookies, withCookies, Cookies } from "react-cookie";
+
 import style from './styles.scss'
 
-const Header = () => {
+const Header = ({props}) => {
+  // const cookies = Cookies
+  const [cookies, setCookie, removeCookie] = useCookies(["userLogin"]);
+  const [isCookieAvailability, setCookieAvailability] = useState(false);
+
+  useEffect(() => {
+    const cookieData = cookies.userLogin || null
+    if(cookieData) {
+      setCookieAvailability(true)
+    } else {
+      setCookieAvailability(false)
+    }
+  }, [])
+
+  function handleCookie() {
+    setCookie("userLogin", "login12345678", {
+      path: "/"
+    });
+    location.reload();
+  }
+
+  const handleRemoveCookie = () => {
+    removeCookie("userLogin")
+    location.reload();
+  }
+
   return (
-    <div className={style.headerSection}>Header Section</div>
+    <div className={style.headerContainer}>
+      <div className={style.headerSection}>Header Section</div>
+      {!isCookieAvailability ? (
+        <button onClick={handleCookie} className={style.cookieButton}>Set Cookie</button>
+      ) : (
+        <button onClick={handleRemoveCookie} className={style.cookieButton}>Remove Cookie</button>
+      )}
+     </div>
   )
 }
 
-export default React.memo(Header);
+export default withCookies(Header);
--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {render} from 'react-dom'
 import {Provider} from 'react-redux'
 import configureStore from './store'
+import { CookiesProvider } from "react-cookie";
 import MainApp from './containers/index.container'
 
 // Create a fresh store
@@ -9,7 +10,9 @@ const store = configureStore()
 
 render(
   <Provider store={store}>
+    <CookiesProvider>
      <MainApp />
+    </CookiesProvider>
   </Provider>,
   document.getElementById('root')
 )
